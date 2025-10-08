5e06ad45f7067f23e31448933e952726ff4709b1
zlatanoski <zlatanoskidavid92@gmail.com>
2025-10-08 21:42:10 +0200

    Grid for current Tasks in the AddTask tab done using Flatlist mapping, currently using dummy data of 4 strings

--- app/ (tabs)/addTask.tsx
+++ app/ (tabs)/addTask.tsx
@@ -1,13 +1,42 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {Component, useMemo} from 'react';
+import {FlatList, Text, View, useWindowDimensions ,Platform} from 'react-native';
+import {AddSkillCard} from "../reusableComponents/addSkillCard";
+
+
+const DATA = ["Instrument","Language","Math","Sport"]
+
 
 
 export default function AddTask() {
+
+    const {width } = useWindowDimensions();
+
+    const numColumns = useMemo(()=>{
+
+        if(width >= 768) return 4;
+        else return 2;
+
+    },[width])
+
+    const gap = 12;
+    const padX = 16
+
+    const cardWidth = useMemo(()=>
+        (width - padX*2 - gap*(numColumns-1)) / numColumns,
+        [width,numColumns]
+    );
+
         return (
-            <View className='flex-1 bg-[#060014]'>
+            <View className='flex-1 bg-[#060014] pt-20'>
+                <View style={{paddingHorizontal: gap}} >
+                <FlatList data={DATA} key={numColumns}  keyExtractor={(item) => item} numColumns={numColumns} columnWrapperStyle={{gap,justifyContent:"flex-start"}} ItemSeparatorComponent={()=><View style={{height:gap}}/>    } renderItem={({item})=> (
 
-                <Text className="">Add Task</Text>
+                    <View style={{width:cardWidth, aspectRatio:1}}>
+                        <AddSkillCard title={item}></AddSkillCard>
+                    </View>
 
+                )} scrollEnabled={false} showsVerticalScrollIndicator={false} contentContainerStyle={{paddingVertical:gap}} />
+                </View>
             </View>
         );
     }
---
+++ app/reusableComponents/addSkillCard.tsx
@@ -0,0 +1,16 @@
+// ExampleScreen.tsx
+import React from 'react';
+import { View,Text,StyleSheet } from 'react-native';
+
+
+export function AddSkillCard(props:{title:string}) {
+    return (
+        <View className=" aspect-square bg-blue-300 border-2 rounded-lg ">
+            <Text>Instrument</Text>
+
+        </View>
+
+    );
+}
+
+
